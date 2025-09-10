0fb2c1ed0e12a7fc5705e74cbe6a3c14aa53a5f1
Farhan Nisar <webprocode139@gmail.com>
2025-09-10 23:39:50 +0530

    button blur

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ const btnNav = document.querySelector(".btn-mobile-nav");
 const header = document.querySelector(".header");
 
 btnNav.addEventListener('click',function(){
-    header.classList.toggle("nav-open")
+    header.classList.toggle("nav-open");
+    btnNav.blur();
 })
