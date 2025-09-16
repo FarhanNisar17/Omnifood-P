e4115abbdf300feabb5db4509535dae7acac067e
Farhan Nisar <webprocode139@gmail.com>
2025-09-16 20:09:21 +0530

    Navigation Fade animation

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const btnNav = document.querySelector(".btn-mobile-nav");
 const header = document.querySelector(".header");
+const navbar = document.querySelector(".main-nav");
+const navLinks = document.querySelectorAll(".main-nav-link");
 const section1 = document.querySelector("#section--1");
 const learnMore = document.querySelector(".btn--scroll");
 
@@ -17,9 +19,9 @@ document
     e.target.blur();
 
     if (e.target.classList.contains("main-nav-link")) {
-      const id = e.target.getAttribute("href"); 
-      console.log(id);  // id has to be specified as thats the location we scrolling to
-      document.querySelector(id).scrollIntoView({ 
+      const id = e.target.getAttribute("href");
+      console.log(id); // id has to be specified as thats the location we scrolling to
+      document.querySelector(id).scrollIntoView({
         behavior: "smooth",
       });
     }
@@ -28,6 +30,22 @@ document
 // Learn More button
 learnMore.addEventListener("click", function () {
   section1.scrollIntoView({
-    behavior: 'smooth',
+    behavior: "smooth",
   });
 });
+
+// Navigation fade animation
+const navHover = function (e) {
+  const l = e.target;
+  const siblings = l.closest(".main-nav").querySelectorAll(".main-nav-link");
+  console.log(siblings);
+  const logo = l.closest(".header").querySelector("img");
+
+  siblings.forEach(element => {
+    if(element!= l) element.style.opacity = this;
+  });
+  logo.style.opacity = this;
+};
+
+navbar.addEventListener("mouseover", navHover.bind(0.5));
+navbar.addEventListener("mouseout", navHover.bind(1));
